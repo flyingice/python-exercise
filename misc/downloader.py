ce293713e10589088ea4ff6d04d3212d405062c3
Yibin YANG <rainforest.vista@gmail.com>
2018-10-07 22:52:19 +0200

    kitten image downloader

---
+++ misc/downloader.py
@@ -0,0 +1,14 @@
+import os.path
+import urllib.request
+import urllib.error
+
+try:
+    width = int(input('image width: '))
+    height = int(input('image height: '))
+
+    url = "http://placekitten.com/{0}/{1}".format(width, height)
+    with urllib.request.urlopen(url) as infile:
+        with open(os.path.expanduser('~/Downloads/kitten.jpg'), 'wb') as outfile:
+            outfile.write(infile.read())
+except (ValueError, urllib.error.URLError, urllib.error.HTTPError, OSError) as err:
+    print(err)
