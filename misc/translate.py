1f6c846385664f22451c9e8b60547ef2dbc1f094
Yibin YANG <rainforest.vista@gmail.com>
2018-10-08 17:49:06 +0200

    Chinese translation via Google

---
+++ misc/translate.py
@@ -0,0 +1,31 @@
+import html.parser
+import re
+import urllib.request
+
+
+def prep_query(text):
+    # replace whitespaces by the separator '+'
+    return '+'.join(text.split())
+
+
+def translate(text, fromLang="auto", toLang="zh-CN"):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+    url = "http://translate.google.cn/m?tl={0}&sl={1}&q={2}".format(
+        toLang, fromLang, html.escape(prep_query(text)))
+    request = urllib.request.Request(url, headers=headers)
+    try:
+        # bytes to utf-8 encoding
+        data = urllib.request.urlopen(request).read().decode("utf-8")
+        regex = r'class="t0">(.*?)<'
+        res = re.findall(regex, data)
+    except Exception as err:
+        print(err)
+
+    return html.unescape(res[0]) if 'res' in locals() and res else ""
+
+
+res = translate(input("Please enter the text to translate: \n"))
+if res:
+    print(res)
