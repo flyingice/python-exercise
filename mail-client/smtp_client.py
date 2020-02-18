30c6a4b368e258674802967d9fdcf86c4a5655bb
Yibin YANG <rainforest.vista@gmail.com>
2020-02-18 15:27:08 +0800

    SMTP client refactoring

--- mail-client/smtp_client.py
+++ mail-client/smtp_client.py
@@ -3,18 +3,11 @@ import ssl
 import configparser
 import base64
 import logging
-import sys
 import re
 
+CRLF = '\r\n'
 BUF_SIZE = 2048
 PORT = 465  # SMTPS, use port 25 for SMTP
-RECIPIENT = 'rainforest.vista@gmail.com'
-MSG = ("To: {}\r\n"
-       "Subject: This is the title\r\n"
-       "\r\n"
-       "This is the 1st line.\r\n"
-       "This is the 2nd line.\r\n"
-       "This is the last line.\r\n").format(RECIPIENT)
 
 
 class SmtpClient:
@@ -23,7 +16,7 @@ class SmtpClient:
 
     def command(self, *args, buf_size=BUF_SIZE, status=None):
         if args:
-            cmd = args[0] + '\r\n'
+            cmd = args[0] + CRLF
             logging.debug(cmd.encode())
             self.sock.send(cmd.encode())
 
@@ -31,9 +24,7 @@ class SmtpClient:
         logging.debug(reply)
         if status and reply.decode()[:3] != str(status):
             logging.error('unexpected reply from the server')
-            return False
-
-        return True
+            raise OSError
 
     def smtp_handshaking(self, host):
         self.command(status=220)
@@ -42,21 +33,19 @@ class SmtpClient:
     def smtp_auth(self, user, passwd):
         self.command('AUTH LOGIN', status=334)
         self.command(base64.b64encode(user.encode()).decode(), status=334)
-        if not self.command(base64.b64encode(passwd.encode()).decode(), status=235):
-            logging.error('check credentials')
-            sys.exit()
+        self.command(base64.b64encode(passwd.encode()).decode(), status=235)
 
-    def smtp_prep(self, sender):
+    def smtp_prep(self, sender, recipient):
         # MAIL FROM
         self.command('MAIL FROM: <{}>'.format(sender), status=250)
         # RCPT TO (multiple RCPT TO commands in case of multiple recipients)
-        self.command('RCPT TO: <{}>'.format(RECIPIENT), status=250)
+        for address in recipient:
+            self.command('RCPT TO: <{}>'.format(address), status=250)
 
-    def smtp_send(self):
+    def smtp_send(self, data):
         # DATA
         self.command('DATA', status=354)
-        if not self.command('{}\r\n.'.format(MSG), status=250):
-            logging.error('failed to send the message')
+        self.command('{0}{1}.'.format(data, CRLF), status=250)
 
     def smtp_quit(self):
         self.command('QUIT', status=221)
@@ -66,40 +55,70 @@ def parse_config(filename):
     config = configparser.ConfigParser()
     with open(filename) as infile:
         config.read_file(infile)
-    return config['DEFAULT']['sender'], config['DEFAULT']['passwd']
+    sender, passwd = config['DEFAULT']['sender'], config['DEFAULT']['passwd']
+    validate_email(sender)
+    return sender, passwd
+
+
+def parse_data(filename):
+    recipient = []
+    lines = []
+    with open(filename) as infile:
+        # parse header subject, to etc..
+        for line in infile:
+            lines.append(line.strip())
+            if lines[-1] == '':
+                break
+            key, value = lines[-1].split(':', maxsplit=1)
+            if key.lower() == 'to':
+                recipient = [address.strip() for address in value.split(',')]
+
+        for address in recipient:
+            validate_email(address)
+
+        lines.extend([line.strip() for line in infile.readlines()])
+
+    return recipient, '{}'.format(CRLF).join(lines)
 
 
 def validate_email(email):
-    return re.match(r'^[\w.-]+@[\w.-]+$', email)
+    if re.match(r'^[\w.-]+@[\w.-]+$', email) is None:
+        raise ValueError
+
+
+def get_user(sender):
+    return sender.split('@', 1)[0]
+
+
+def get_host(sender):
+    return 'smtp.{}'.format(sender.split('@', 1)[1])
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
 
     try:
-        sender, passwd = parse_config('./user.conf')
-    except (OSError, KeyError, configparser.MissingSectionHeaderError) as err:
-        logging.error(err)
-        sys.exit()
+        sender, passwd = parse_config('etc/user.conf')
+        recipient, data = parse_data("etc/data.txt")
 
-    if not validate_email(sender):
-        logging.error('invalid sender mail address')
-        sys.exit()
+        host = get_host(sender)
+        logging.info('connecting to {0}:{1}...'.format(host, PORT))
+        context = ssl.create_default_context()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            with context.wrap_socket(sock, server_hostname=host) as ssock:
+                ssock.connect((host, PORT))
+                logging.info(
+                    "connection established with {0}".format(ssock.version()))
 
-    user, domain = sender.split('@', 1)[0], sender.split('@', 1)[1]
-    host = 'smtp.' + domain
-
-    logging.info('connecting to {0}:{1}...'.format(host, PORT))
-    context = ssl.create_default_context()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        with context.wrap_socket(sock, server_hostname=host) as ssock:
-            ssock.connect((host, PORT))
-            logging.info(
-                "connection established with {0}".format(ssock.version()))
-
-            smtp_client = SmtpClient(ssock)
-            smtp_client.smtp_handshaking(host)
-            smtp_client.smtp_auth(user, passwd)
-            smtp_client.smtp_prep(sender)
-            smtp_client.smtp_send()
-            smtp_client.smtp_quit()
+                smtp_client = SmtpClient(ssock)
+                smtp_client.smtp_handshaking(host)
+                smtp_client.smtp_auth(get_user(sender), passwd)
+                smtp_client.smtp_prep(sender, recipient)
+                smtp_client.smtp_send(data)
+                smtp_client.smtp_quit()
+    except ValueError as err:
+        logging.error("invalid email address\r\n{}".format(err))
+    except (KeyError, configparser.MissingSectionHeaderError) as err:
+        logging.error("invalid config file\r\n{}".format(err))
+    except OSError as err:
+        logging.error("smtp failure\r\n{}".format(err))
--- mail-client/smtp_test.py
+++ mail-client/smtp_test.py
@@ -5,24 +5,26 @@ import configparser
 
 class TestSMTPClient(unittest.TestCase):
     def test_parse_config(self):
-        sender, _ = smtp_client.parse_config('./user.conf')
+        sender, _ = smtp_client.parse_config('etc/user.conf')
         self.assertEqual(sender, 'flyingicefr@163.com')
 
     def test_parse_config_exception(self):
         with self.assertRaises(OSError):
-            smtp_client.parse_config('./nonexist.conf')
+            smtp_client.parse_config('etc/nonexist.conf')
 
         with self.assertRaises(KeyError):
-            smtp_client.parse_config('./invalid_key.conf')
+            smtp_client.parse_config('etc/invalid_key.conf')
 
         with self.assertRaises(configparser.MissingSectionHeaderError):
-            smtp_client.parse_config('./missing_header.conf')
+            smtp_client.parse_config('etc/missing_header.conf')
 
     def test_validate_email(self):
-        self.assertTrue(smtp_client.validate_email('abc.def@gmail.com'))
-        self.assertFalse(smtp_client.validate_email('abc'))
-        self.assertFalse(smtp_client.validate_email('@163.com'))
-        self.assertFalse(smtp_client.validate_email('xyz@123@hotmail.com'))
+        smtp_client.validate_email('abc.def@gmail.com')
+
+        with self.assertRaises(ValueError):
+            smtp_client.validate_email('abc')
+            smtp_client.validate_email('@163.com')
+            smtp_client.validate_email('xyz@123@hotmail.com')
 
 
 if __name__ == '__main__':
